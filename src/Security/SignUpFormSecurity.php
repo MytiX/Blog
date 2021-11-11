11bca48891325fd11b144273930ca4954beb0a47
MytiX <ferreirajose84700@gmail.com>
2021-11-11 14:29:56 +0100

    Build regex for the form Sign up

--- src/Security/SignUpFormSecurity.php
+++ src/Security/SignUpFormSecurity.php
@@ -9,11 +9,11 @@ class SignUpFormSecurity extends FormSecurity
     protected array $configInput = [
         'pseudoInput' => [
             'isNull' => false,
-            'constraint' => '/()/'
+            'constraint' => '/^([a-zA-Z0-9_]{4,16})$/'
         ],
         'emailInput' => [
             'isNull' => false,
-            'constraint' => ''
+            'constraint' => '/^[a-zA-Z0-9-_.]+[@]{1}[a-zA-Z0-9-]+[.]{1}[a-z]{2,4}$/'
         ],
         'passwordInput' => [
             'isNull' => false,

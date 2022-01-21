73a90ef3a5098b1f23b84f6febade0048a1d44cc
MyTiX <ferreirajose84700@gmail.com>
2022-01-21 16:46:51 +0100

    Update contact form component

--- template/components/contactForm.php
+++ template/components/contactForm.php
@@ -1,18 +1,23 @@
 <div class="row justify-content-around">
     <div class="col-12 col-md-8">
+        <?php if (!empty($successFlash = $session->flash('successFlash'))):?>
+            <small class="text-success"><?= $successFlash ?></small>
+        <?php endif; ?>
+    </div>
+    <div class="col-12 col-md-8">
         <h3 class="blog-post-title mb-4 border-bottom-custom pb-4">Contactez-moi !</h3>
         <form action="#" method="post" class="pb-4">
             <div class="form-group pb-2">
                 <label for="emailInput">Votre adresse mail :</label>
-                <input type="email" class="form-control mt-2" id="emailInput" name="emailInput" value="" required>
+                <input type="email" class="form-control mt-2" id="emailInput" name="emailInput" value="<?= (!empty($form['emailInput'])) ? $form['emailInput'] : '' ?>" required>
             </div>
             <div class="form-group pb-2">
                 <label for="objetInput">Quel est l'objet de votre message ?</label>
-                <input type="text" class="form-control mt-2" id="objetInput" name="objetInput" value="" required>
+                <input type="text" class="form-control mt-2" id="objetInput" name="objetInput" value="<?= (!empty($form['objetInput'])) ? $form['objetInput'] : '' ?>" required>
             </div>
             <div class="form-group pb-2">
                 <label for="messageInput">Votre message :</label>
-                <textarea class="form-control mt-2" id="messageInput" name="messageInput" rows="3" required></textarea>
+                <textarea class="form-control mt-2" id="messageInput" name="messageInput" rows="3" required><?= (!empty($form['messageInput'])) ? $form['messageInput'] : '' ?></textarea>
             </div>
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="checkInput" name="checkInput" value="1" required>

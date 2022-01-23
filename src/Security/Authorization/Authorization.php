d54ce6cc64bca59b9c53bf4518176f8e091af91a
MyTiX <ferreirajose84700@gmail.com>
2022-01-23 19:10:21 +0100

    Create Authorization class and add logic on class Application

---
+++ src/Security/Authorization/Authorization.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security\Authorization;
+
+use Config\AppConfig;
+use App\Core\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class Authorization
+{
+    public static function access(array $roles): bool|RedirectResponse
+    {
+        if (empty($roles)) {
+            return true;
+        }
+
+        $session = Session::getSession();
+
+        if (empty($user = $session->get(AppConfig::USER_SESSION))) {
+            return new RedirectResponse(AppConfig::URL.'/signin');
+        }
+
+        if (!in_array($user['role'], $roles)) {
+            return new RedirectResponse(AppConfig::URL);
+        }
+        return true;
+    }
+}

90002a7f44fc912e43316575f5ef55c984f55d66
MyTiX <ferreirajose84700@gmail.com>
2022-01-28 18:39:40 +0100

    Add logic pagination on AllPostController

--- src/Controller/AllPostController.php
+++ src/Controller/AllPostController.php
@@ -12,16 +12,38 @@ class AllPostController extends AbstractController
     #[Route('/all-post')]
     public function index(): Response
     {
-        $page = 1;
+        $request = $this->getRequest();
+
+        $page = 0;
         $limit = 2;
+        $moreResult = false;
+
+        if (!empty($r = $request->query->get('page')) && is_numeric($r) && $r > 0) {
+            $page = $r - 1;
+        }
 
         $posts = (new Posts())->findBy([
             'params' => [
                 'active' => 1
             ],
+            'limit' => $limit + 1,
+            'offset' => $page * $limit,
+            'orderBy' => [
+                'id DESC'
+            ],
         ]);
+
+        if (null !== $posts && count($posts) > $limit) {
+            $moreResult = true;
+            unset($posts[array_key_last($posts)]);
+        }
+
+        // dd($posts, $moreResult, $limit, $page * $limit);
+
         return $this->render('/post/all-post.php', [
             'posts' => $posts,
+            'moreResult' => $moreResult,
+            'page' => $page + 1,
         ]);
     }
 }

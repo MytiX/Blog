8cd556f17f54b81c6db95237a5e4beff38fa6475
MyTiX <ferreirajose84700@gmail.com>
2022-02-02 16:17:14 +0100

    Change key isNull through nullable

--- src/Security/Form/CommentsFormSecurity.php
+++ src/Security/Form/CommentsFormSecurity.php
@@ -9,7 +9,7 @@ class CommentsFormSecurity extends FormSecurity
     public array $configInput = [
         'content' => [
             'type' => 'string',
-            'isNull' => false,
+            'nullable' => false,
             'constraint' => '/^.{1,500}$/',
             'constraintError' => 'La longueur du commentaire est trop longue 500 caractères maximum',
         ],

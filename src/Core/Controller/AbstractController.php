bb19ed2156ca1a2662d72e3e24a4020920c4ced4
MyTiX <ferreirajose84700@gmail.com>
2022-02-04 10:27:44 +0100

    AbstractController PHP Doc

--- src/Core/Controller/AbstractController.php
+++ src/Core/Controller/AbstractController.php
@@ -11,7 +11,15 @@ abstract class AbstractController
 {
     private Request $request;
 
-    protected function render(string $file, array $args = [], int $statusCode = 200)
+    /**
+     * render
+     * Return Response with the view
+     * @param  string $file
+     * @param  array $args
+     * @param  int $statusCode
+     * @return Response
+     */
+    protected function render(string $file, array $args = [], int $statusCode = 200): Response
     {
         $template = new Templating();
 
@@ -22,16 +30,32 @@ abstract class AbstractController
         return new Response($template->getView($file, $args), $statusCode);
     }
 
-    public function setRequest(Request $request)
+    /**
+     * setRequest
+     * Set request used on class Application
+     * @param  mixed $request
+     * @return void
+     */
+    public function setRequest(Request $request): void
     {
         $this->request = $request;
     }
 
+    /**
+     * getRequest
+     * Return Http foundation Request
+     * @return Request
+     */
     protected function getRequest(): Request
     {
         return $this->request;
     }
 
+    /**
+     * getSession
+     * Return a Session object is a Singleton
+     * @return Session
+     */
     protected function getSession(): Session
     {
         return Session::getSession();
